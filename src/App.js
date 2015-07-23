ffeb6c443882702898128a1c361977deeecec505
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-07-24 00:02:54 +0530

    First component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
-alert("Hello!");
+var React = require('react');
+
+var App = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Open Library</h1>
+        <h3> Books </h3>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
