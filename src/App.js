05dc323d391c25dab2d483a42662e7d7dc8fe8d1
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-07-23 23:59:41 +0530

    Init - Say hello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,1 @@
-var React = require('react');
-
-var App = React.createClass({
-  render() {
-    return (
-      <h1>Welcome in React world.</h1>
-    );
-  }
-});
-
-module.exports = App;
+alert("Hello!");
