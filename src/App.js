6f2b425c42bb7e9099a9fce34e7dfc7cb892951f
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-07-24 11:30:03 +0530

    Displayed list of books using map

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 var React = require('react');
 
-var Book = {
-  name: 'jQuery in Action',
-  author: 'John Resig'
-};
+var BookList = [{ name: 'jQuery in Action',
+                  author: 'John Resig',
+                  cover: 'https://covers.openlibrary.org/b/olid/OL25431312M-S.jpg'},
+                {name: 'Head first jQuery',
+                 author: 'Ryan Benedetti',
+                 cover: 'https://archive.org/download/headfirstjquery00bene/page/cover_w60_h60.jpg'}];
 
 var App = React.createClass({
+  _renderBook(book) {
+    return (
+      <div className="book-container">
+        <img src={book.cover}
+             className="book-cover"
+             height="100" />
+        <div>
+          <h2>
+            {book.name}
+          </h2>
+          <h3 className="book-author">
+            by {book.author}
+          </h3>
+        </div>
+      </div>
+    );
+  },
+
   render() {
+    var books = BookList.map((book) => {
+      return this._renderBook(book);
+    });
+
     return (
       <div>
         <h1>Open Library</h1>
         <h3> Books </h3>
-
-        <div className="book-container">
-          <img src="https://covers.openlibrary.org/b/olid/OL9701406M-S.jpg"
-               className="book-cover"
-               height="100" />
-          <div>
-            <h2>Lord of the Rings</h2> by
-            <h3 className="book-author"> Doug Moe </h3>
-          </div>
-        </div>
-
+        {books}
       </div>
     );
   }
